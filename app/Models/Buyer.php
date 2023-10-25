37a7652e07246009f2820072962707a2de9c502f
permalik <permalik@protonmail.com>
2023-10-25 14:09:48 -0500

    1|1: Username|Buyer

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Buyer extends Model
 {
     use HasFactory;
+
+    public function username(): BelongsTo
+    {
+        return $this->belongsTo(Username::class);
+    }
 }
--- app/Models/Username.php
+++ app/Models/Username.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Username extends Model
 {
     use HasFactory;
+
+    public function buyer(): HasOne
+    {
+        return $this->belongsTo(Buyer::class);
+    }
 }
