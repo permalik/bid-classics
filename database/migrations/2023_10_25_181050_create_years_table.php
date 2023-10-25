e069aa4d7cf6d013e8b890cb3dfd503be439bdd6
permalik <permalik@protonmail.com>
2023-10-25 13:16:25 -0500

    fix: name on years table should be unique

--- database/migrations/2023_10_25_181050_create_years_table.php
+++ database/migrations/2023_10_25_181050_create_years_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('years', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 4);
+            $table->string('name', 4)->unique();
             $table->timestamps();
         });
     }
