3f6d4de252a91e4eb4d383b22c16dc7e7b9dbe2d
permalik <permalik@protonmail.com>
2023-10-28 14:48:17 -0500

    reset: remove breeze/vue/inertia implementation

---
+++ resources/js/Layouts/BaseLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import {Link} from '@inertiajs/vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+  <main class="flex min-h-screen flex-col items-center bg-neutral-800">
+    <slot />
+  </main>
+</template>
