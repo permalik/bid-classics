a9b87f0b9d6c0b1d4931c4c5681277dc1058c687
permalik <permalik@protonmail.com>
2023-10-29 23:48:48 -0500

    style: update 404 and FontFamil

--- resources/js/Pages/404.vue
+++ resources/js/Pages/404.vue
@@ -8,18 +8,29 @@
         <h1>
             Skrrrrr...
         </h1>
+        <p>
+            <span>
+                Seems like you're off the beaten path.
+            </span>
+            <span>
+                This page isn't available.
+            </span>
+        </p>
     </div>
 </template>
 
 <style scoped>
-.wrapper {
-    position: relative;
-}
 img {
     max-height: 100vh;
 
     position: absolute;
     top: 0;
     right: 0;
+}
+.wrapper {
+    position: relative;
+}
+p > span {
+    display: table;
 }
 </style>
