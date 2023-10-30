65a4983a74ef147c87df79272f5d8d3eeeda1e72
permalik <permalik@protonmail.com>
2023-10-30 09:34:43 -0500

    style: Content section of Welcome view

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,34 +23,80 @@ defineProps({
 <template>
     <Head title="Home"/>
     <Header/>
-    <div v-if="canLogin">
-        <Link
-            v-if="$page.props.auth.user"
-            :href="route('dashboard')"
-            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-        >Dashboard
-        </Link
-        >
-
-        <template v-else>
+    <main>
+        <section>
+            <h1>
+            <span>
+                Build a Car Collection
+            </span>
+                <span>
+                Worth Coveting
+            </span>
+            </h1>
+            <p>
+                <span>
+                    Discover your favorites today.
+                </span>
+                <span>
+                    Build barn finds from the bottom up or
+                </span>
+                <span>
+                    cruise in a mint cherry of your dreams.
+                </span>
+            </p>
+        </section>
+        <div v-if="canLogin">
             <Link
-                :href="route('login')"
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-            >Log in
+            >Dashboard
             </Link
             >
 
-            <Link
-                v-if="canRegister"
-                :href="route('register')"
-                class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-            >Register
-            </Link
-            >
-        </template>
-    </div>
+            <template v-else>
+                <Link
+                    :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Log in
+                </Link
+                >
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Register
+                </Link
+                >
+            </template>
+        </div>
+    </main>
 </template>
 
-<style>
+<style scoped>
+main {
+    margin: clamp(5rem, 15vw, 20rem) 0 0 clamp(.125rem, 5vw, 15rem);
 
+    display: flex;
+    flex-flow: column nowrap;
+}
+
+h1 > span {
+    display: table;
+
+    font-size: clamp(1.5rem, 3vw, 2.5rem);
+    font-weight: 800;
+    line-height: clamp(1.75rem, 4vw, 3rem);
+}
+
+p {
+    opacity: .8;
+    font-size: clamp(0.95rem, 3vw, 1.5rem);
+    font-weight: 600;
+}
+
+p > span {
+    display: table;
+}
 </style>
