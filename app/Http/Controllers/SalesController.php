0e60b55f270cbb68fd0e14b6e55203339cdd491d
permalik <permalik@protonmail.com>
2023-10-29 20:17:17 -0500

    refac: clean up SalesController

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -16,35 +16,8 @@ class SalesController extends Controller
             'vehicle.classic',
         )->get();
 
-        $years = [];
-        $makes = [];
-        $classics = [];
-        $askingPrices = [];
-        $vehicleIds = [];
-//
-//        foreach ($sales as $sale) {
-//            array_push($years, $sale->vehicle->year->name);
-//        }
-//        foreach ($sales as $sale) {
-//            array_push($makes, $sale->vehicle->make->name);
-//        }
-//        foreach ($sales as $sale) {
-//            array_push($classics, $sale->vehicle->classic->name);
-//        }
-//        foreach ($sales as $sale) {
-//            array_push($askingPrices, $sale->asking_price);
-//        }
-//        foreach ($sales as $sale) {
-//            array_push($vehicleIds, $sale->vehicle_id);
-//        }
-
         return Inertia::render('Sales', [
             'sales' => $sales,
-//            'years' => $years,
-//            'makes' => $makes,
-//            'classics' => $classics,
-//            'askingPrices' => $askingPrices,
-//            'vehicleIds' => $vehicleIds,
         ]);
     }
 }
