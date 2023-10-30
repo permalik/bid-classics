8db02a33bf2c21d18761ee76db32e61f3cb1d434
permalik <permalik@protonmail.com>
2023-10-30 11:26:51 -0500

    style: InputLabel

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -7,8 +7,19 @@ defineProps({
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label>
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
 </template>
+
+<style scoped>
+label {
+    display: block;
+
+    font-size: .8rem;
+    font-weight: 600;
+    color: var(--primary-light);
+    opacity: .7;
+}
+</style>
