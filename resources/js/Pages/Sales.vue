5ea2e68f64690b2ace1a34a256e5f0f14cf8be03
permalik <permalik@protonmail.com>
2023-10-30 14:18:46 -0500

    style: Sales view

--- resources/js/Pages/Sales.vue
+++ resources/js/Pages/Sales.vue
@@ -1,58 +1,100 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import {Link} from '@inertiajs/vue3';
+import Header from "@/Components/Header.vue";
 defineProps({
     sales: Array,
-    years: Array,
-    makes: Array,
-    classics: Array,
-    askingPrices: Array,
-    asking_price: Number,
-    vehicle: Number,
-    classic: String,
-    name: String,
-    year_id: Number,
     sale: Object,
     id: Number,
+    vehicle: Number,
+    name: String,
+    asking_price: Number,
+    classic: String,
     make: Object,
 })
 </script>
 
 <template>
-    <ul>
-        <li v-for="sale of sales">
-        <Link :href="'/sale/' + sale.id">
-            <span>
-                {{ sale.vehicle.year.name }}
-            </span>
-            <br/>
-            <span>
-                {{ sale.vehicle.make.name }}
-            </span>
-            <br/>
-            <span>
-                {{ sale.vehicle.classic.name }}
-            </span>
-            <br/>
-            <span>
-                {{ sale.asking_price }}
-            </span>
-        </Link>
-        </li>
-<!--        <li>-->
-<!--            <a href="/vehicle/{{ id }}">-->
-<!--                <span v-for="year of years">-->
-<!--                    {{ year }}-->
-<!--                </span>-->
-<!--                <span v-for="make of makes">-->
-<!--                    {{ make }}-->
-<!--                </span>-->
-<!--                <span v-for="classic of classics">-->
-<!--                    {{ classic }}-->
-<!--                </span>-->
-<!--                <span v-for="askingPrice of askingPrices">-->
-<!--                    {{ askingPrice }}-->
-<!--                </span>-->
-<!--            </a>-->
-<!--        </li>-->
-    </ul>
+    <Header />
+    <main>
+        <section>
+            <h1>
+                All Vehicles
+            </h1>
+            <ul>
+                <li v-for="sale of sales" class="vehicle-item">
+                    <Link :href="'/sale/' + sale.id" class="vehicle-link">
+                        <span class="vehicle-price">
+                            ${{ sale.asking_price }}
+                        </span>
+                        <div class="vehicle-name">
+                            <span>
+                                {{ sale.vehicle.year.name }}
+                            </span>
+                            <span>
+                                {{ sale.vehicle.make.name }}
+                            </span>
+                            <span>
+                                {{ sale.vehicle.classic.name }}
+                            </span>
+                        </div>
+                    </Link>
+                </li>
+            </ul>
+        </section>
+    </main>
 </template>
+
+<style scoped>
+main {
+    min-height: 100vh;
+
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+}
+
+section {
+    width: 100%;
+    max-width: 40rem;
+    margin: 2rem 0;
+    padding: 0 0.5rem;
+}
+
+h1 {
+    margin: 0 0 1.5rem 0;
+
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+
+ul > li:not(:last-child) {
+    margin: 0 0 1rem 0;
+}
+
+.vehicle-item:hover {
+    scale: 1.01;
+}
+
+.vehicle-link {
+    padding: 0.25rem 0.5rem;
+
+    display: flex;
+    flex-flow: column nowrap;
+
+    background-color: var(--primary-dark-800);
+    border-radius: 5px;
+}
+
+.vehicle-price {
+    font-weight: 600;
+    color: var(--tertiary-accent);
+}
+
+.vehicle-name {
+    margin: 0.5rem 0 0 0;
+}
+
+.vehicle-name > span:not(:last-child) {
+    margin: 0 0.25rem 0 0;
+}
+</style>
