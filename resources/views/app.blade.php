3e69778aa7ce2d55a9441b553df7df897c8f1264
permalik <permalik@protonmail.com>
2023-10-30 17:06:21 -0500

    chore: update favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
         <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900|lobster:400" rel="stylesheet" />
+        <link
+            rel='icon'
+            href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚘</text></svg>'
+        />
 
         <!-- Scripts -->
         @routes
