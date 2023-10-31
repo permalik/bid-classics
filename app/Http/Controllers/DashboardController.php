facc30f646c3478e7833bb78c6037ebc0b5cc414
permalik <permalik@protonmail.com>
2023-10-31 08:41:48 -0500

    feat: Implement Dashboard view. Propagate data and complete styles

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Vehicle;
 use Inertia\Inertia;
+use Auth;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
+        $vehicles = Vehicle::with('year', 'make', 'classic')->get();
+        $currentUserId = Auth::id();
+        $prunedVehicles = [];
+
+        foreach ($vehicles as $vehicle) {
+            if ($vehicle->is_sold && $vehicle->user_id == $currentUserId) {
+                array_push($prunedVehicles, $vehicle);
+            }
+        }
+
         return Inertia::render('Dashboard', [
-            'data' => 'data'
+            'vehicles' => $prunedVehicles
         ]);
     }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,17 +1,70 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
+
+defineProps({
+    vehicles: Array,
+})
 </script>
 
 <template>
     <Head title="Dashboard"/>
     <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
+        <div class="wrapper">
+            <section>
+                <h1>
+                    Your Garage
+                </h1>
+                <ul>
+                    <li v-for="vehicle of vehicles" class="vehicle-item">
+                    <span>
+                        {{ vehicle.year.name }}
+                    </span>
+                        <span>
+                        {{ vehicle.make.name }}
+                    </span>
+                        <span>
+                        {{ vehicle.classic.name }}
+                    </span>
+                    </li>
+                </ul>
+            </section>
         </div>
     </AuthenticatedLayout>
 </template>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+}
+
+section {
+    width: 100%;
+    max-width: 40rem;
+    margin: 5rem 0;
+    padding: 0 0.5rem;
+}
+
+h1 {
+    margin: 0 0 1.5rem 0;
+
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+
+ul > li:not(:last-child) {
+    margin: 0 0 1rem 0;
+}
+
+.vehicle-item {
+    padding: 0.25rem 0.5rem;
+
+    display: flex;
+    flex-flow: column nowrap;
+
+    background-color: var(--primary-dark-800);
+    border-radius: 5px;
+}
+</style>
